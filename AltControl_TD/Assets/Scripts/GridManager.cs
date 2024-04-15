3cd302411c73278835c3959277e1435c23aabe06
GameCrucible <cpdepalm@gmail.com>
2024-04-15 15:11:10 -0500

    Tower Update
    Fixed all the tower Stuff, not have limited tower to correlate with physical controller.

--- AltControl_TD/Assets/Scripts/GridManager.cs
+++ AltControl_TD/Assets/Scripts/GridManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
     private GameObject _currentTower;
     [SerializeField] private GameObject[] _towerArray;
+    private bool _isDeleteOn = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         _currentTower = _towerArray[0];
@@ -15,18 +16,14 @@ public class GridManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("1"))
-        {
-            _currentTower = _towerArray[0];
-        }
-        if (Input.GetKeyDown("2"))
-        {
-            _currentTower = _towerArray[1];
-        }
-        if (Input.GetKeyDown("3"))
-        {
-            _currentTower = _towerArray[2];
-        }
+        if (Input.GetKeyDown("1")){_currentTower = _towerArray[0]; _isDeleteOn = false; }
+
+        if (Input.GetKeyDown("2")){_currentTower = _towerArray[1]; _isDeleteOn = false; }
+
+        if (Input.GetKeyDown("3")){_currentTower = _towerArray[2]; _isDeleteOn = false; }
+
+        if(Input.GetKeyDown("4")) { _isDeleteOn = true; Debug.Log(_isDeleteOn); }
+        
         //Check for mouse click 
         if (Input.GetMouseButtonDown(0))
         {
@@ -45,15 +42,25 @@ public class GridManager : MonoBehaviour
 
     public void CurrentClickedGameObject(GameObject gameObject)
     {
+        
         if (gameObject.tag == "Tile")
         {
             Debug.Log("hit the griddy");
             Tile tileScript = gameObject.GetComponent<Tile>();
             if (!tileScript.CheckIsOccupied())
             {
-                Instantiate(_currentTower, new Vector3(gameObject.transform.position.x, 1.075f, gameObject.transform.position.z), Quaternion.identity);
-                tileScript.ChangeIsOccupied();
+                _currentTower.transform.position = new Vector3(gameObject.transform.position.x, 1.0f, gameObject.transform.position.z);
+               tileScript.ChangeIsOccupied();
+               tileScript.ChangeTower(_currentTower);
             }
+
+        }
+        else if(gameObject.tag == "Tower")
+        {
+            if (_isDeleteOn) {
+                gameObject.transform.position = new Vector3(0f,-5f,0f);
+            }
+            
         }
     }
 }
